34eb05c50b85fe9e684f3796249defea23e8c585
Chris <electronicsleep@gmail.com>
2019-07-24 20:17:35 -0700

    spring-boot-beans

--- src/main/java/springbootjava/Application.java
+++ src/main/java/springbootjava/Application.java
@@ -10,15 +10,18 @@ import org.springframework.context.ApplicationContext;
 public class Application {
     
     public static void main(String[] args) {
-        ApplicationContext ctx = SpringApplication.run(Application.class, args);
-        
-        System.out.println("Let's inspect the beans provided by Spring Boot:");
+        //ApplicationContext ctx = SpringApplication.run(Application.class, args);
+        SpringApplication.run(Application.class, args);
+
+        /*
+        System.out.println("Beans provided by Spring Boot:");
         
         String[] beanNames = ctx.getBeanDefinitionNames();
         Arrays.sort(beanNames);
         for (String beanName : beanNames) {
             System.out.println(beanName);
         }
+        */
     }
 
 }
