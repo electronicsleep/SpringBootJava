1f9f10b4e3c819fcf4f133eec75626200655bdc0
Chris <electronicsleep@gmail.com>
2022-02-02 21:23:14 -0800

    update-resources-dir

--- docker-run.sh
+++ docker-run.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
-set -e
-gradle build
 set +e
 docker rm springbootjava
 set -e
+gradle build
 docker build -t springbootjava .
 docker run -t -p 8080:8080 --name springbootjava -it springbootjava
